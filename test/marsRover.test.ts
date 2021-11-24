04980e4ba69fe1705345b0205827cc9aafc3f587
Maldorana <maldorana@gmail.com>
2021-11-24 20:18:30 +0000

    Test passed to rotate the rover to the right

--- test/marsRover.test.ts
+++ test/marsRover.test.ts
@@ -1,8 +1,16 @@
 import { Rover } from '../src/Rover';
 
 describe('Mars Rover Test', () => {
-  it('should initialize the rover', () => {
-    const rover = new Rover();
-    expect(rover.execute('')).toBe('0,0:N');
+  describe('Initialisation', () => {
+    it('should initialise the rover', () => {
+      const rover = new Rover();
+      expect(rover.execute('')).toBe('0,0:N');
+    });
+  });
+  describe('Rotating', () => {
+    it('should rotate the rover to the right (90°)', () => {
+      const rover = new Rover();
+      expect(rover.execute('R')).toBe('0,0:E');
+    });
   });
 });
