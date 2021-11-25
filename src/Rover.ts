0f5bbcee8aca03b52f2925bf0da47a51fb671993
Maldorana <maldorana@gmail.com>
2021-11-25 10:25:32 +0000

    Refactored with Position class

--- src/Rover.ts
+++ src/Rover.ts
@@ -1,15 +1,16 @@
 import { Direction } from './Direction';
+import { Position } from './Position';
 
 export class Rover {
   private direction = new Direction();
-  private y = 0;
+  private position = new Position(0, 0);
 
   execute(commands: string): string {
     for (const command of commands) {
       if (command === 'R') this.direction.rotateRight();
       if (command === 'L') this.direction.rotateLeft();
-      if (command === 'M') this.y++;
+      if (command === 'M') this.position.y++;
     }
-    return `${this.y},0:${this.direction}`;
+    return `${this.position.y},${this.position.x}:${this.direction}`;
   }
 }
