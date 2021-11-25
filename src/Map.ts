26e6a0010ef5ae0fd13b59a9f8db62be40479c38
Maldorana <maldorana@gmail.com>
2021-11-25 11:43:00 +0000

    Refactored with Map class

---
+++ src/Map.ts
@@ -0,0 +1,15 @@
+import { Direction } from './Direction';
+import { Position } from './Position';
+
+export class Map {
+  moveNext(position: Position, direction: Direction): Position {
+    let y = position.y;
+    let x = position.x;
+
+    if (direction.toString() === 'N') y++;
+    if (direction.toString() === 'E') x++;
+    if (direction.toString() === 'S') y--;
+
+    return new Position(y, x);
+  }
+}
--- src/Rover.ts
+++ src/Rover.ts
@@ -1,18 +1,18 @@
 import { Direction } from './Direction';
 import { Position } from './Position';
+import { Map } from './Map';
 
 export class Rover {
   private direction = new Direction();
   private position = new Position(0, 0);
+  private map = new Map();
 
   execute(commands: string): string {
     for (const command of commands) {
       if (command === 'R') this.direction.rotateRight();
       if (command === 'L') this.direction.rotateLeft();
       if (command === 'M') {
-        if (this.direction.toString() === 'N') this.position.y++;
-        if (this.direction.toString() === 'E') this.position.x++;
-        if (this.direction.toString() === 'S') this.position.y--;
+        this.position = this.map.moveNext(this.position, this.direction);
       }
     }
     return `${this.position}:${this.direction}`;
